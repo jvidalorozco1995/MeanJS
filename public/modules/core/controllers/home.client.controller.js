58954cf70947b04eddeca4ab4d1d686329b3acc0
jvprestige <jorgevidal199548@gmail.com>
2015-07-25 23:29:24 -0500

    Modificando el controlador

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,61 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+		$scope.alerts = [
+			{
+             	icon:'glyphicon-user',
+				colour:'btn-success',
+				total:'20,438',
+				description:'TOTAL CLIENTES'
+			},
+
+			{
+				icon:'glyphicon-calendar',
+				colour:'btn-primary',
+				total:'20,438',
+				description:'TOTAL CLIENTES'
+
+			},
+
+			{
+				icon:'glyphicon-edit',
+				colour:'btn-success',
+				total:'20,438',
+				description:'TOTAL CLIENTES'
+
+			},
+
+			{
+				icon:'glyphicon-record',
+				colour:'btn-success',
+				total:'20,438',
+				description:'TOTAL CLIENTES'
+
+			},
+
+
+			{
+				icon:'glyphicon-eye-open',
+				colour:'btn-warning',
+				total:'20,438',
+				description:'TOTAL CLIENTES'
+
+			},
+
+
+			{
+				icon:'glyphicon-flag',
+				colour:'btn-danger',
+				total:'20,438',
+				description:'TOTAL CLIENTES'
+
+			}
+
+
+		];
+
+
+
 	}
 ]);
